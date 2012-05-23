129861004e0a04ccf5ba255ad16d2bbeb6e897a0
Carl Brasic <carl.brasic@pearson.com>
2012-05-23 15:48:23 -0500

    typo (should be a hash)

--- mvundos.rb
+++ mvundos.rb
@@ -16,9 +16,9 @@ origfiles = Find.find(PREFIX_DIR).to_a.
 #make an array of file names and targets
 # e.g. [ ['/home/brasca/.file1.un~', '/home/brasca/.vim/undo/%home%brasca%file1'],
 #        ['/home/brasca/.file2.un~', '/home/brasca/.vim/undo/%home%brasca%file2'] ]
-newfiles = origfiles.map do |x|
+newfiles = Hash[origfiles.map do |x|
   [ x, VIM_UNDO_DIR + x.gsub(/\/\.?/,'%').gsub('.un~','') ]
-end
+end]
 
 cmds = newfiles.map do |src,dst|
   puts "mv #{src} #{dst}"
